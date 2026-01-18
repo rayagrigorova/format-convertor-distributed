35176f99752f246cde75a59afa15f65910bffecb
Raya <raiagrigorova@gmail.com>
2026-01-18 11:46:02 +0200

    Remove node_modules and Jest leftovers

--- jest.setup.js
+++
@@ -1,9 +0,0 @@
-require("./public/convert.js");
-
-global.DT = global.DataTransformer;
-
-global.jsyaml = require("js-yaml");
-
-if (global.NodeList && !NodeList.prototype.forEach) {
-  NodeList.prototype.forEach = Array.prototype.forEach;
-}
